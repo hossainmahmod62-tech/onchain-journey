4d9668f4c9ebe1201bf49498451240c539a6c1d6
Ryujikage <hossainmahmod62@gmail.com>
2026-09-15 01:40:31 +0600

    Update tokenConfig.js

--- src/lib/tokenConfig.js
+++ src/lib/tokenConfig.js
@@ -1,1 +1,21 @@
 
+// Stablecoin configuration
+
+export const TOKENS = {
+  ethereum: [
+    {
+      symbol: "USDC",
+      name: "USD Coin",
+      decimals: 6,
+      contract:
+        "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
+    },
+    {
+      symbol: "USDT",
+      name: "Tether USD",
+      decimals: 6,
+      contract:
+        "0xdAC17F958D2ee523a2206206994597C13D831ec7",
+    },
+  ],
+};
