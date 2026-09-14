30b7652cc0eb8d82fab80dae7bde605b1eb710f7
Ryujikage <hossainmahmod62@gmail.com>
2026-09-15 02:15:17 +0600

    Update etherscanClient.js

--- src/lib/etherscanClient.js
+++ src/lib/etherscanClient.js
@@ -1,1 +1,27 @@
 
+const API_URL = "https://api.etherscan.io/v2/api";
+
+export async function fetchTokenTransfers(address) {
+  const apiKey = import.meta.env.VITE_ETHERSCAN_API_KEY;
+
+  const params = new URLSearchParams({
+    chainid: "1",
+    module: "account",
+    action: "tokentx",
+    address,
+    page: "1",
+    offset: "100",
+    sort: "asc",
+    apikey: apiKey,
+  });
+
+  const response = await fetch(`${API_URL}?${params}`);
+
+  const data = await response.json();
+
+  if (data.status !== "1") {
+    return [];
+  }
+
+  return data.result;
+  }
